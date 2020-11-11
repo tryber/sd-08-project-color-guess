787f5ad7d4da4cb7178db78c9282993807ebcc85
wesleypribeiro <wesley_paulo00@hotmail.com>
2020-11-11 17:26:48 -0300

    R03 - Adicionado o requisito 03

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let rgb_color_p = document.querySelector('#rgb-color');
 rgb_color_p.innerText = randomColor();
+let randomPos = parseInt(Math.random() * 6);
+
+randomPallete();
 
 function randomColor() { // Gera uma cor aleatória
     const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
@@ -7,4 +10,13 @@ function randomColor() { // Gera uma cor aleatória
     const g = randomBetween(0, 255);
     const b = randomBetween(0, 255);
     return `(${r}, ${g}, ${b})`;
+}
+
+function randomPallete() { // Gera uma paleta randomica de cores
+    let balls = document.querySelectorAll('.ball');
+    for (index of balls) {
+        index.style.backgroundColor = `rgb${randomColor()}`;
+    }
+
+    balls[randomPos].style.backgroundColor = `rgb${randomColor}`;
 }
