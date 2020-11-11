feb95358cd3ac408a94fb809c921c12dae20a182
emassaki <massakihirayama@gmail.com>
2020-11-11 18:22:21 -0300

    Adicionando requisito 3 - Criar os 6 circulos para as cores

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 // Gera um código RGB
 let rgb;
 function generateRGB() {
-  const gessColor = document.getElementById('rgb-color');
+  const gessColor = document.getElementById("rgb-color");
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  rgb = `(${r}, ${g}, ${b})`
+  rgb = `(${r}, ${g}, ${b})`;
   gessColor.innerHTML = rgb;
 }
 generateRGB();
+
+function createColor(numberOfBalls) {
+  for (let ball = 0; ball < numberOfBalls; ball += 1) {
+    const colorContainer = document.getElementById('color-container');
+    let color = document.createElement('div');
+    color.className = 'ball';
+    colorContainer.appendChild(color);
+  }
+}
+createColor(6)
