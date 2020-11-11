0622e70147ac98a11e8d2f7aa38c2b5ed535598f
MEianelli <filipelouzano@gmail.com>
2020-11-11 17:06:37 -0300

    Tudo feito

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 window.onload = function () {
   gerarBolas();
+  document.querySelector("#reset-game").addEventListener("click", gerarBolas);
 };
 
 function gerarBolas() {
   let ballsDiv = document.querySelector(".ballsDiv");
+  let resposta = document.querySelector("#answer");
+  resposta.innerHTML = "Escolha uma cor";
+  ballsDiv.innerHTML = "";
 
   let escolha = Math.floor(Math.random() * 6);
 
@@ -30,6 +34,7 @@ function checaSeAcertou(corClicada, corCerta) {
   let resposta = document.querySelector("#answer");
   if (corClicada == corCerta) {
     resposta.innerHTML = "Acertou!";
+    incScore();
   } else {
     resposta.innerHTML = "Errou! Tente novamente!";
   }
@@ -45,4 +50,11 @@ function gerarCorAleatoria() {
   let c2 = Math.floor(Math.random() * 255);
   let c3 = Math.floor(Math.random() * 255);
   return "rgb(" + c1 + ", " + c2 + ", " + c3 + ")";
+}
+
+function incScore() {
+  let score = document.querySelector("#score");
+  let scoreNumber = parseInt(score.innerHTML);
+  scoreNumber += 3;
+  score.innerHTML = scoreNumber;
 }
