0f1e474d94ce2bb966fb524ac2aef529678f9562
emassaki <massakihirayama@gmail.com>
2020-11-11 19:03:44 -0300

    Adicionado requisito 4 - Colorir os circulos com cores aleatorias

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 // Gera um código RGB
 let rgb;
+const gessColor = document.getElementById("rgb-color");
 function generateRGB() {
-  const gessColor = document.getElementById("rgb-color");
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  rgb = `(${r}, ${g}, ${b})`;
+  createColor(1);
   gessColor.innerHTML = rgb;
 }
 generateRGB();
+// Cria as cores
+function createColor(numberOfColors) {
+  let colors = [];
+  for (let color = 0; color < numberOfColors; color += 1) {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    rgb = `RGB(${r}, ${g}, ${b})`;
+    colors.push(rgb);
+  }
+  return colors;
+}
 
-function createColor(numberOfBalls) {
+// Gera os círculos para receberem as cores
+function createColorBall(numberOfBalls) {
   for (let ball = 0; ball < numberOfBalls; ball += 1) {
     const colorContainer = document.getElementById('color-container');
     let color = document.createElement('div');
@@ -18,4 +28,18 @@ function createColor(numberOfBalls) {
     colorContainer.appendChild(color);
   }
 }
-createColor(6)
+createColorBall(6)
+
+// Insere as cores nos círculos
+function insertColors() {
+  // Define um array com as cores e troca um dos valores do array pelo valor sorteado 
+  let ballColors = createColor(6);
+  let rightColor = Math.floor(Math.random() * 6);
+  ballColors[rightColor] = gessColor.innerHTML;
+  // Insere as cores
+  const balls = document.querySelectorAll('.ball');
+  for (let index = 0; index < ballColors.length; index += 1) {
+    balls[index].style.backgroundColor = ballColors[index];
+  }
+}
+insertColors()
