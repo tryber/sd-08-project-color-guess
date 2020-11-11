798a19eafc199c0fe0508465a5566ba34442bc2a
Igor Nascimento <igorln96@gmail.com>
2020-11-11 19:43:04 -0300

    req5: criando evento de click certo

--- script.js
+++ script.js
@@ -17,6 +17,19 @@ let rgbColorRight = "rgb(" + r + ", " + g + ", " + b + ")"
 let rgbTitle = document.getElementById('rgb-color');
 rgbTitle.innerHTML = "(" + r + ", " + g + ", " + b + ")";
 
+// Cria as 6 bolas
+let ballsCase = document.getElementById('ballsCase');
+function createBalls () {
+    for (let i = 0; i < 6; i += 1) {
+    let ball = document.createElement('div');
+    ball.className = "ball";
+    ball.addEventListener('click', check)
+    ballsCase.appendChild(ball);
+    }
+}
+
+createBalls()
+
 // Colore as 6 bolas
 let ball = document.getElementsByClassName('ball')
 function color () {
@@ -38,5 +51,13 @@ function colorRight () {
 
 colorRight()
 
-
+// Acertou ou errou a cor
+let answer = document.getElementById("answer");
+function check (event) {
+    if (event.target.style.backgroundColor === rgbColorRight) {
+        answer.innerHTML = "Acertou!"
+    } else {
+        answer.innerHTML = "Errou! Tente novamente!"
+    }
+}
 
