ed303b2ec453d0f1cc8790a4bb8615d00a393869
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2020-11-11 23:35:11 -0300

    Complete all requirements

--- script.js
+++ script.js
@@ -1,7 +1,59 @@
+function randomNumber(range) {
+  return Math.floor(Math.random() * range);
+}
+
 function randomColor() {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    let rgbColor = `rgb(${r},${g},${b})`;
-    return rgbColor;
+  return `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
+}
+
+let ballsContainer = document.getElementById("balls-container");
+function createBalls() {
+  for (let index = 0; index < 6; index += 1) {
+    let newBall = document.createElement("div");
+    newBall.className = "ball";
+    ballsContainer.appendChild(newBall);
   }
+}
+createBalls();
+
+let balls = document.querySelectorAll(".ball");
+function colorBalls() {
+  for (let arrayPosition = 0; arrayPosition < balls.length; arrayPosition += 1) {
+    balls[arrayPosition].style.backgroundColor = randomColor();
+  }
+}
+colorBalls();
+
+let rgbNumber = document.getElementById("rgb-color");
+function rgbNumberName() {
+  rgbNumber.innerText = balls[randomNumber(6)].style.backgroundColor;
+}
+rgbNumberName();
+
+let answerMessage = document.getElementById("answer");
+function checkBallrgbNumber() {
+  let scoreMessage = document.getElementById("score");
+  let score = 0;
+  ballsContainer.addEventListener("click", function(event){
+    if (event.target.style.backgroundColor == rgbNumber.innerText) {
+      answerMessage.innerText = "Acertou!";
+      score += 3;
+      scoreMessage.innerText = score;
+      colorBalls();
+      rgbNumberName();
+    } else {
+      answerMessage.innerText = "Errou! Tente novamente!";
+    }
+  })
+}
+checkBallrgbNumber();
+
+function resetGame() {
+  let resetButton = document.getElementById("reset-game");
+  resetButton.addEventListener("click", function(){
+    colorBalls();
+    rgbNumberName();
+    answerMessage.innerText = "Escolha uma cor";
+  })
+}
+resetGame();
