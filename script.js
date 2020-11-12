2567aaf0267b8a595e25447fc01845cbcb4ddd0a
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2020-11-12 00:31:11 -0300

    Fix some code climate issues

--- script.js
+++ script.js
@@ -3,57 +3,63 @@ function randomNumber(range) {
 }
 
 function randomColor() {
-  return `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
+  return `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(
+    256
+  )})`;
 }
 
-let ballsContainer = document.getElementById("balls-container");
+const ballsContainer = document.getElementById('balls-container');
 function createBalls() {
   for (let index = 0; index < 6; index += 1) {
-    let newBall = document.createElement("div");
-    newBall.className = "ball";
+    const newBall = document.createElement('div');
+    newBall.className = 'ball';
     ballsContainer.appendChild(newBall);
   }
 }
 createBalls();
 
-let balls = document.querySelectorAll(".ball");
+const balls = document.querySelectorAll('.ball');
 function colorBalls() {
-  for (let arrayPosition = 0; arrayPosition < balls.length; arrayPosition += 1) {
+  for (
+    let arrayPosition = 0;
+    arrayPosition < balls.length;
+    arrayPosition += 1
+  ) {
     balls[arrayPosition].style.backgroundColor = randomColor();
   }
 }
 colorBalls();
 
-let rgbNumber = document.getElementById("rgb-color");
+const rgbNumber = document.getElementById('rgb-color');
 function rgbNumberName() {
   rgbNumber.innerText = balls[randomNumber(6)].style.backgroundColor;
 }
 rgbNumberName();
 
-let answerMessage = document.getElementById("answer");
+const answerMessage = document.getElementById('answer');
 function checkBallrgbNumber() {
-  let scoreMessage = document.getElementById("score");
+  const scoreMessage = document.getElementById('score');
   let score = 0;
-  ballsContainer.addEventListener("click", function(event){
-    if (event.target.style.backgroundColor == rgbNumber.innerText) {
-      answerMessage.innerText = "Acertou!";
+  ballsContainer.addEventListener('click', function (event) {
+    if (event.target.style.backgroundColor === rgbNumber.innerText) {
+      answerMessage.innerText = 'Acertou!';
       score += 3;
       scoreMessage.innerText = score;
       colorBalls();
       rgbNumberName();
     } else {
-      answerMessage.innerText = "Errou! Tente novamente!";
+      answerMessage.innerText = 'Errou! Tente novamente!';
     }
-  })
+  });
 }
 checkBallrgbNumber();
 
 function resetGame() {
-  let resetButton = document.getElementById("reset-game");
-  resetButton.addEventListener("click", function(){
+  const resetButton = document.getElementById('reset-game');
+  resetButton.addEventListener('click', function () {
     colorBalls();
     rgbNumberName();
-    answerMessage.innerText = "Escolha uma cor";
-  })
+    answerMessage.innerText = 'Escolha uma cor';
+  });
 }
 resetGame();
