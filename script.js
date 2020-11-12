0fec2fe8d98db3e37b44d7458502084c7df0f259
emassaki <massakihirayama@gmail.com>
2020-11-11 22:26:22 -0300

    Adicionado requisito 7 - Pontuaçao ao acertar

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-// Gera um código RGB
 let rgb;
-const gessColor = document.getElementById("rgb-color");
-function generateNewChallenge() {
-  createRGBColor(1);
-  gessColor.innerHTML = rgb;
-}
-generateNewChallenge();
-// Cria as cores
+const gessColor = document.getElementById('rgb-color');
 function createRGBColor(numberOfColors) {
-  let colors = [];
+  const colors = [];
   for (let color = 0; color < numberOfColors; color += 1) {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
@@ -18,46 +11,48 @@ function createRGBColor(numberOfColors) {
   }
   return colors;
 }
+function generateNewChallenge() {
+  createRGBColor(1);
+  gessColor.innerHTML = rgb;
+}
+generateNewChallenge();
 
-// Gera os círculos para receberem as cores
 const colorContainer = document.getElementById('color-container');
 function createEmptyBall(numberOfBalls) {
   for (let ball = 0; ball < numberOfBalls; ball += 1) {
-    let color = document.createElement('div');
+    const color = document.createElement('div');
     color.className = 'ball';
     colorContainer.appendChild(color);
   }
 }
-createEmptyBall(6)
+createEmptyBall(6);
 
-// Insere as cores nos círculos
 function insertColors() {
-  // Define um array com as cores e troca um dos valores do array pelo valor sorteado 
-  let ballColors = createRGBColor(6);
-  let rightColor = Math.floor(Math.random() * 6);
+  const ballColors = createRGBColor(6);
+  const rightColor = Math.floor(Math.random() * 6);
   ballColors[rightColor] = gessColor.innerHTML;
-  // Insere as cores
   const balls = document.querySelectorAll('.ball');
   for (let index = 0; index < ballColors.length; index += 1) {
     balls[index].style.backgroundColor = ballColors[index];
   }
 }
-insertColors()
+insertColors();
 
-// Apresentar o resultado
 const answer = document.getElementById('answer');
 colorContainer.addEventListener('click', (event) => {
-  if (event.target.style.backgroundColor === gessColor.innerHTML) {  
+  if (event.target.style.backgroundColor === gessColor.innerHTML) {
     answer.innerHTML = 'Acertou!';
+    const score = document.getElementById('score');
+    const points = parseInt(score.innerHTML, 10) + 3;
+    score.innerHTML = points;
   } else {
     answer.innerHTML = 'Errou! Tente novamente!';
   }
 });
 
-// Reseta o jogo
-const reset = document.getElementById('reset-game');
-reset.addEventListener('click', () => {
+const resetButton = document.getElementById('reset-game');
+resetButton.addEventListener('click', () => {
   generateNewChallenge();
   insertColors();
   answer.innerHTML = 'Escolha uma cor';
-})
+});
