24fbc0b45cc0bb66d5cf5683f0c5b8f38ae5cb02
Ivanildo Cândido <systechtecnologia@gmail.com>
2020-11-14 16:52:58 -0300

    Sending mandatory requirement 5

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-function generateColorRGB() {
-  const red = Math.floor(Math.random() * 255);
-  const green = Math.floor(Math.random() * 255);
-  const blue = Math.floor(Math.random() * 255);
-  return `rgb(${red} , ${green} , ${blue})`;
+function generateColorRGB(numberOfColors) {
+  const assortedColors = [];
+  for (let indexColor = 0; indexColor < numberOfColors; indexColor += 1) {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    assortedColors.push(`(${red}, ${green}, ${blue})`);
+  }
+  return assortedColors;
 }
 function createBallElements(numberOfBalls) {
   const sectionBalls = document.getElementById('color-control');
-  for (let indexBall = 1; indexBall <= numberOfBalls; indexBall += 1) {
+  const chosenColor = document.getElementById('chosen-color');
+  const assortedColors = generateColorRGB(numberOfBalls);
+  chosenColor.innerText = assortedColors[Math.floor(Math.random() * 6)];
+  for (let indexBall = 0; indexBall < numberOfBalls; indexBall += 1) {
     const divBall = document.createElement('div');
     divBall.classList.add('ball');
-    divBall.style.backgroundColor = generateColorRGB();
+    divBall.style.backgroundColor = `rgb${assortedColors[indexBall]}`;
     sectionBalls.appendChild(divBall);
+  }
+}
+function checkAttempt() {
+  const balls = document.querySelectorAll('.ball');
+  const chosenColor = document.getElementById('chosen-color');
+  const answer = document.getElementById('answer');
+  for (let indexBall = 0; indexBall < balls.length; indexBall += 1) {
+    balls[indexBall].addEventListener('click', function (event) {
+      if (
+        event.target.style.backgroundColor === `rgb${chosenColor.innerText}`
+      ) {
+        answer.innerText = 'Acertou!';
+      } else {
+        answer.innerText = 'Errou! Tente novamente!';
+      }
+    });
   }
 }
 window.onload = function () {
   createBallElements(6);
+  checkAttempt();
 };
