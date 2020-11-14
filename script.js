48b6ee8185e49436925c2fb6932a89c29403278e
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2020-11-14 18:03:22 -0300

    Requisito 5 e 6

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const balls = document.getElementsByClassName('ball');
+const textColor = document.getElementById('rgb-color');
+const textAnswer = document.getElementById('answer');
+const buttonReset = document.getElementById('reset-game')
+const colors = [];
 
 function randomNumber() {
     const number = Math.floor(Math.random() *256);
@@ -10,6 +14,30 @@ function randomColor() {
     return rgb;
 }
 
-for(let ball = 0; balls.length; ball ++) {
-    balls[ball].style.backgroundColor = randomColor();
+function clickBall(event) {
+    const selectedColor = event.target.style.backgroundColor;
+    if (selectedColor == textColor.textContent) {
+        textAnswer.textContent = 'Acertou!';
+    } else {
+        textAnswer.textContent = 'Errou! Tente novamente!';
+    }
+}
+
+for (let ball = 0; balls.length; ball += 1) {
+    const color = randomColor();
+    balls[ball].style.backgroundColor = color;
+    colors[ball] = color;
+    balls[ball].addEventListener('click', clickBall);
 };
+
+function namedRandomColor() {
+    textColor.textContent = colors[randomColor(6)];
+}
+
+namedRandomColor();
+
+function resetGame() {
+    window.location.reload();
+}
+
+buttonReset.addEventListener('click', resetGame);
