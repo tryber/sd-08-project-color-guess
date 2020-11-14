43621452997e18f5e33dc87214196321b118c043
Ivanildo Cândido <systechtecnologia@gmail.com>
2020-11-14 15:49:18 -0300

    Sending mandatory requirement 4

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+function generateColorRGB() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  return `rgb(${red} , ${green} , ${blue})`;
+}
 function createBallElements(numberOfBalls) {
   const sectionBalls = document.getElementById('color-control');
   for (let indexBall = 1; indexBall <= numberOfBalls; indexBall += 1) {
     const divBall = document.createElement('div');
     divBall.classList.add('ball');
+    divBall.style.backgroundColor = generateColorRGB();
     sectionBalls.appendChild(divBall);
   }
 }
-
 window.onload = function () {
   createBallElements(6);
 };
