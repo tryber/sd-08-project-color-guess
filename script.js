09b528f4d9afa95db406141723e446ba27309ccf
Ediberto Oliveira <edibertooliveira@aol.com>
2020-11-17 14:39:20 -0300

    feat: question 5, 6, 7

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const getColorSelected = document.getElementById('rgb-color');
 const getOptionColor = document.getElementById('option-color');
+const getResetGame = document.getElementById('reset-game');
+const setMessageGame = document.getElementById('answer');
+const setScoreGame = document.getElementById('score');
+let score = 0;
 let arrColor = [];
 let colorSelected = '';
 
@@ -8,22 +12,50 @@ const createColorRandom = () => {
     const getColorRandom1 = Math.ceil(Math.random() * 255);
     const getColorRandom2 = Math.ceil(Math.random() * 255);
     const getColorRandom3 = Math.ceil(Math.random() * 255);
-    arrColor.push(`${getColorRandom1}, ${getColorRandom2}, ${getColorRandom3}`);
+    arrColor.push(
+      `(${getColorRandom1}, ${getColorRandom2}, ${getColorRandom3})`
+    );
   }
   colorSelected = arrColor[Math.ceil(Math.random() * arrColor.length) - 1];
 };
 const createElementHTML = () => {
   for (let index = 0; index < 6; index += 1) {
-    getOptionColor.innerHTML += `<div class="ball" style="background-color:rgb(${arrColor[0]})"></div>`;
+    getOptionColor.innerHTML += `<div class="ball" style="background-color: rgb${arrColor[index]}"></div>`;
   }
 };
-const setSelected = () => {
-  getColorSelected.innerText = `RGB(${colorSelected})`;
+const removeElementHTMLAll = () => {
+  document.querySelectorAll('.ball').forEach((element) => element.remove());
+  arrColor = [];
+  colorSelected = '';
+  setMessageGame.innerText = '"Escolha uma cor';
 };
-window.onload = function () {
+const setSelected = () => {
+  getColorSelected.innerText = `${colorSelected}`;
+};
+const checked = (colorClick, colorSelected) => {
+  const colorCurrent = colorClick.replace('rgb', '');
+  console.log(colorSelected);
+  if (colorCurrent === colorSelected) {
+    score += 3;
+    setScoreGame.innerText = score;
+    return (setMessageGame.innerText = 'Acertou!');
+  }
+  // score -= 1;
+  setMessageGame.innerText = 'Errou! Tente novamente!';
+};
+const eventClickCircle = () => {
+  document.querySelectorAll('.ball').forEach((element) => {
+    element.addEventListener('click', (event) => {
+      let colorClick = event.target.style.backgroundColor;
+      checked(colorClick, colorSelected);
+    });
+  });
+};
+
+getResetGame.addEventListener('click', () => {
+  removeElementHTMLAll();
   createColorRandom();
   createElementHTML();
   setSelected();
-  console.log(arrColor);
-  console.log(colorSelected);
-};
+  eventClickCircle();
+});
