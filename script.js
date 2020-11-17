e3e442bb5fbfaebfa1ea0b88f5279ee37d805b5e
Ediberto Oliveira <edibertooliveira@aol.com>
2020-11-17 15:42:45 -0300

    fix: codeclimate

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const getColorSelected = document.getElementById('rgb-color');
-const getOptionColor = document.getElementById('option-color');
 const getResetGame = document.getElementById('reset-game');
 const setMessageGame = document.getElementById('answer');
 const setScoreGame = document.getElementById('score');
@@ -7,26 +6,6 @@ let score = 0;
 let arrColor = [];
 let colorSelected = '';
 
-const createColorRandom = () => {
-  for (let index = 0; index < 6; index += 1) {
-    const getColorRandom1 = Math.ceil(Math.random() * 255);
-    const getColorRandom2 = Math.ceil(Math.random() * 255);
-    const getColorRandom3 = Math.ceil(Math.random() * 255);
-    arrColor.push(`(${getColorRandom1}, ${getColorRandom2}, ${getColorRandom3})`);
-  }
-  colorSelected = arrColor[Math.ceil(Math.random() * arrColor.length) - 1];
-};
-const createElementHTML = () => {
-  for (let index = 0; index < 6; index += 1) {
-    getOptionColor.innerHTML += `<div class="ball" style="background-color: rgb${arrColor[index]}"></div>`;
-  }
-};
-const removeElementHTMLAll = () => {
-  document.querySelectorAll('.ball').forEach((element) => element.remove());
-  arrColor = [];
-  colorSelected = '';
-  setMessageGame.innerText = '"Escolha uma cor';
-};
 const checked = (colorClick, colorSelect) => {
   const colorCurrent = colorClick.replace('rgb', '');
   if (colorCurrent === colorSelect) {
@@ -34,7 +13,6 @@ const checked = (colorClick, colorSelect) => {
     setScoreGame.innerText = score;
     return (setMessageGame.innerText = 'Acertou!');
   }
-  // score -= 1;
   return (setMessageGame.innerText = 'Errou! Tente novamente!');
 };
 const eventClickCircle = () => {
@@ -45,14 +23,38 @@ const eventClickCircle = () => {
     });
   });
 };
+
+const createColorRandom = () => {
+  for (let index = 0; index < 6; index += 1) {
+    const getColorRandom1 = Math.ceil(Math.random() * 255);
+    const getColorRandom2 = Math.ceil(Math.random() * 255);
+    const getColorRandom3 = Math.ceil(Math.random() * 255);
+    arrColor.push(`(${getColorRandom1}, ${getColorRandom2}, ${getColorRandom3})`);
+  }
+  colorSelected = arrColor[Math.ceil(Math.random() * arrColor.length) - 1];
+};
+const paintElementCicleColor = () => {
+  document.querySelectorAll('.ball').forEach((element, index) => {
+    element.style.backgroundColor = `rgb${arrColor[index]}`;
+  });
+  eventClickCircle();
+};
 const setSelected = () => {
   getColorSelected.innerText = `${colorSelected}`;
 };
 
-getResetGame.addEventListener('click', () => {
-  removeElementHTMLAll();
+const resetGame = () => {
+  arrColor = [];
+  colorSelected = '';
+  setMessageGame.innerText = '"Escolha uma cor';
   createColorRandom();
-  createElementHTML();
+  paintElementCicleColor();
   setSelected();
-  eventClickCircle();
-});
+};
+
+window.onload = function () {
+  createColorRandom();
+  paintElementCicleColor();
+  setSelected();
+  getResetGame.addEventListener('click', resetGame);
+};
