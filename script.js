979543135ce38fcd38b5ba9764ea0f52fde2e287
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-17 23:40:02 -0300

    requisito 4: corrige erros codeclimate

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const divBall = document.getElementsByClassName('ball')
+const divBall = document.getElementsByClassName('ball');
 
 function randomNumber() {
-    const numberRandom = Math.floor(Math.random() * 256);
-    return numberRandom;
-};
+	const numberRandom = Math.floor(Math.random() * 256);
+	return numberRandom;
+}
 
 function randomColor() {
-    const rgb = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
-    return rgb;
-};
+	const rgb = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+	return rgb;
+}
 
-console.log(randomColor());
-console.log(divBall)
-
-for (let ball = 0; ball < divBall.length; ball++) {
-    divBall[ball].style.backgroundColor = randomColor();
+for (let ball = 0; ball < divBall.length; ball += 1) {
+	divBall[ball].style.backgroundColor = randomColor();
 }
