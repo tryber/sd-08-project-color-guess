b44d937249a9422af543b173dea4eda38ad1d2e1
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2020-11-18 00:55:27 -0300

    requisito 4 - cores dinamicas na paletta

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function randomColor() {
-  const rgbColor = document.querySelector('#rgb-color');
   const red = Math.floor(Math.random() * 255);
   const green = Math.floor(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
   const stringColor = `(${red}, ${green}, ${blue})`;
-  rgbColor.innerHTML = stringColor;
+  return stringColor;
 }
 
 randomColor();
@@ -13,9 +12,20 @@ function createPalette() {
   for (let index = 0; index < 6; index += 1) {
     const ballColor = document.createElement('div');
     ballColor.className = 'ball';
+    ballColor.style.backgroundColor = `rgb${randomColor()}`;
     const palettecolor = document.querySelector('#palette-color');
     palettecolor.appendChild(ballColor);
   }
 }
 
 createPalette();
+
+function questColor() {
+  const colors = document.querySelectorAll('.ball');
+  const randomNumber = Math.floor(Math.random() * 5.99);
+  const color = colors[randomNumber].style.backgroundColor;
+  const rgbColor = document.querySelector('#rgb-color');
+  rgbColor.innerHTML = color.slice(3);
+}
+
+questColor();
