966ebca6f1c45f1c26eb453197251567bc9fa7b1
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-17 23:52:31 -0300

    code climate

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const divBall = document.getElementsByClassName('ball');
 
 function randomNumber() {
-	const numberRandom = Math.floor(Math.random() * 256);
-	return numberRandom;
+  const numberRandom = Math.floor(Math.random() * 256);
+  return numberRandom;
 }
 
 function randomColor() {
-	const rgb = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
-	return rgb;
+  const rgb = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+  return rgb;
 }
 
 for (let ball = 0; ball < divBall.length; ball += 1) {
-	divBall[ball].style.backgroundColor = randomColor();
+  divBall[ball].style.backgroundColor = randomColor();
 }
