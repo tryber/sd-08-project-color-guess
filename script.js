50d20ad4106d5fbc3ecd22666ab95a45898266fa
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-11-18 00:32:49 -0300

    Correções codeclimate

--- script.js
+++ script.js
@@ -1,71 +1,66 @@
-window.onload = startFunctions;
+const rgbText = document.querySelector('#rgb-color');
+const balls = document.querySelectorAll('.ball');
+const answerText = document.querySelector('#answer');
 
-function startFunctions(){
-    ballSetup();
-    guessColorText();
-    restartButtonListener();
-    ballsListener();
+function guessColorText() {
+  const randomBall = balls[Math.round(Math.random() * 5)];
+  rgbText.innerHTML = removeRgbText(randomBall);
 }
 
-function guessColorText(){
-    let rgbText = document.querySelector('#rgb-color');
-    let balls = document.querySelectorAll('.ball');
-    let randomBall = balls[Math.round(Math.random()*5)];
-    rgbText.innerHTML = removeRgbText(randomBall);
-}
-
-function ballSetup(){
-    let balls = document.querySelectorAll('.ball');    
-    for(let index = 0; index < balls.length; index += 1){
-        let newRgb = generateRandomRgb();
-        balls[index].style.backgroundColor = "rgb" + newRgb;
-    }
+function ballSetup() {
+  for (let index = 0; index < balls.length; index += 1) {
+    const newRgb = 'rgb' + generateRandomRgb();
+    balls[index].style.backgroundColor = newRgb;
+  }
 }
 
 function ballsListener() {
-    let balls = document.querySelectorAll('.ball');    
-        for(let index = 0; index < balls.length; index += 1){
-        balls[index].addEventListener('click', function(event){
-            setAnswerText(event.target);
-        })
-    }
+  for (let index = 0; index < balls.length; index += 1) {
+    balls[index].addEventListener('click', function (event) {
+      setAnswerText(event.target);
+    });
+  }
 }
 
-function setAnswerText(ballColor){
-    let rightColor = document.querySelector('#rgb-color').innerHTML;
-    let userAnswer = removeRgbText(ballColor);
-    let answerText = document.querySelector('#answer');
-    if(rightColor == userAnswer){
-        let score = document.querySelector('#score')
-        score.innerHTML = parseInt(score.innerHTML) + 3;
-        answerText.innerHTML = "Acertou!";
-    }else{
-        answerText.innerHTML = "Errou! Tente novamente!";
-    }
+function setAnswerText(ballColor) {
+  const rightColor = rgbText.innerHTML;
+  const userAnswer = removeRgbText(ballColor);
+  if (rightColor === userAnswer) {
+    const score = document.querySelector('#score');
+    score.innerHTML = parseInt((score.innerHTML), 10) + 3;
+    answerText.innerHTML = 'Acertou!';
+  } else {
+    answerText.innerHTML = 'Errou! Tente novamente!';
+  }
 }
 
-function restartButtonListener(){
-    let restartButton = document.querySelector('#reset-game');
-    restartButton.addEventListener('click', function(){
-        let answerText = document.querySelector('#answer');
-        answerText.innerHTML = "Escolha uma cor";
-        ballSetup();
-        guessColorText();
-    })
+function restartButtonListener() {
+  const restartButton = document.querySelector('#reset-game');
+  restartButton.addEventListener('click', function () {
+    answerText.innerHTML = 'Escolha uma cor';
+    ballSetup();
+    guessColorText();
+  });
 }
 
 function generateRandomRgb() {
-    let r, g, b;    
-    r = randomNumberForRgb();
-    g = randomNumberForRgb();
-    b = randomNumberForRgb();
-    return `(${r}, ${g}, ${b})`;
+  const r = randomNumberForRgb();
+  const g = randomNumberForRgb();
+  const b = randomNumberForRgb();
+  return `(${r}, ${g}, ${b})`;
 }
 
 function randomNumberForRgb() {
-    return Math.round(Math.random()*255);
+  return Math.round(Math.random() * 255);
 }
 
-function removeRgbText(element){
-    return element.style.backgroundColor.replace('rgb','');
+function removeRgbText(element) {
+  return element.style.backgroundColor.replace('rgb', '');
+}
+
+window.onload = function(){
+  ballSetup();
+  guessColorText();
+  restartButtonListener();
+  ballsListener();
 }
