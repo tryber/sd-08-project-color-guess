e40c98524969a4d7be72b002354b04a87021b50a
Pedro Amaral <amaralph1995@gmail.com>
2020-11-22 20:55:56 -0300

    Req 5 - Retorna resposta se circulo clicado é o correto

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     let pergunta = document.getElementById('rgb-color');
+    let pontuacao = 0
     
     function gera3Aleatorios255() {
         let saida = '(';
@@ -13,11 +14,35 @@ window.onload = function() {
         }
         return saida;
     }
-    pergunta.innerHTML = gera3Aleatorios255();
+    let corPergunta = gera3Aleatorios255();
+    pergunta.innerHTML = corPergunta;
 
     let listaDeBolas = document.querySelectorAll('.ball');
-    for (var i = 0; i < 6; i += 1) {
-        listaDeBolas[i].style.backgroundColor = 'rgb' + (String(gera3Aleatorios255()));
+    function geraBolasColoridas() {
+        let escolhida = Math.floor(Math.random() * 6);
+        console.log(escolhida);
+        for (let i = 0; i < 6; i += 1) {
+            if (i == escolhida) {
+                listaDeBolas[i].style.backgroundColor = 'rgb' + corPergunta;
+                listaDeBolas[i].id = 'resposta'
+            } else {
+                listaDeBolas[i].style.backgroundColor = 'rgb' + (String(gera3Aleatorios255()));
+            }   
+        }
     }
+    geraBolasColoridas()
+
+    let resp = document.querySelector('#answer')
+    let bolaClicada = document.querySelector('#resposta')
+    addEventListener('click', function (event) {
+        let clicada = event.target;
+        if (clicada.id == 'resposta') {
+            pontuacao += 1;
+            resp.innerHTML = pontuacao;
+            alert('Acertou!')
+        } else {
+            alert('Errou! Tente novamente!')
+        }
+    })
 }
 
