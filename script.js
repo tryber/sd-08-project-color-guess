9fa05e1d256ddf936d6f6eae3b97551a159e7df8
Doug-Fernando <doug.fernando95@gmail.com>
2020-12-24 19:32:38 -0300

    Adiciona req 5

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const selectColors = document.querySelectorAll('.ball');
 const rbgColor = document.querySelector('#rgb-color');
+const answer = document.querySelector('#answer');
+
 const randomColor = () => {
   const generateColor = Math.floor(Math.random() * 255);
   return generateColor;
 };
 
-window.onload = () => {
-  selectColors.forEach((ball) => {
-    const redColor = randomColor();
-    const greenColor = randomColor();
-    const blueColor = randomColor();
-    ball.style.backgroundColor = `rgb( ${redColor}, ${greenColor}, ${blueColor} )`;
-  });
+selectColors.forEach((ball) => {
+  const redColor = randomColor();
+  const greenColor = randomColor();
+  const blueColor = randomColor();
+  ball.style.backgroundColor = `rgb( ${redColor}, ${greenColor}, ${blueColor} )`;
 
-  const getRandomColor = Math.floor(Math.random() * selectColors.length);
-  const refColor = selectColors[getRandomColor].style.backgroundColor;
-  rbgColor.innerHTML = refColor.slice(3, 18);
-};
+  ball.addEventListener('click', (event) => {
+    if (event.target.style.backgroundColor === refColor) {
+      answer.innerHTML = 'Acertou!';
+    } else {
+      answer.innerHTML = 'Errou! Tente novamente!';
+    }
+  });
+});
+
+const getRandomColor = Math.floor(Math.random() * selectColors.length);
+const refColor = selectColors[getRandomColor].style.backgroundColor;
+rbgColor.innerHTML = refColor.slice(3, 18);
