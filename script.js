268d8ab1252450f2165ac294734c99328ce333a2
Doug-Fernando <doug.fernando95@gmail.com>
2020-12-24 19:50:34 -0300

    Adiciona req 6

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 const selectColors = document.querySelectorAll('.ball');
 const rbgColor = document.querySelector('#rgb-color');
 const answer = document.querySelector('#answer');
+const resetBtn = document.querySelector('#reset-game');
 
 const randomColor = () => {
   const generateColor = Math.floor(Math.random() * 255);
   return generateColor;
 };
 
-selectColors.forEach((ball) => {
-  const redColor = randomColor();
-  const greenColor = randomColor();
-  const blueColor = randomColor();
-  ball.style.backgroundColor = `rgb( ${redColor}, ${greenColor}, ${blueColor} )`;
+const startGame = () => {
+  if (document.querySelector('.selected') !== null) {
+    document.querySelector('.selected').classList.remove('selected');
+  }
+  selectColors.forEach((ball) => {
+    const redColor = randomColor();
+    const greenColor = randomColor();
+    const blueColor = randomColor();
+    ball.style.backgroundColor = `rgb( ${redColor}, ${greenColor}, ${blueColor} )`;
 
-  ball.addEventListener('click', (event) => {
-    if (event.target.style.backgroundColor === refColor) {
+    ball.addEventListener('click', (event) => {
+      const selectedColor = document.querySelector('.selected');
+      if (!selectedColor) {
+        event.target.classList.add('selected');
+      } else {
+        selectedColor.classList.remove('selected');
+        event.target.classList.add('selected');
+      }
+      checkColor(event.target);
+    });
+  });
+  const getRandomColor = Math.floor(Math.random() * selectColors.length);
+  const refColor = selectColors[getRandomColor].style.backgroundColor;
+  rbgColor.innerHTML = refColor.slice(3, 18);
+
+  const checkColor = (element) => {
+    if (element.style.backgroundColor === refColor) {
       answer.innerHTML = 'Acertou!';
     } else {
       answer.innerHTML = 'Errou! Tente novamente!';
     }
-  });
-});
+  };
+};
 
-const getRandomColor = Math.floor(Math.random() * selectColors.length);
-const refColor = selectColors[getRandomColor].style.backgroundColor;
-rbgColor.innerHTML = refColor.slice(3, 18);
+resetBtn.addEventListener('click', startGame);
+
+window.onload = startGame();
